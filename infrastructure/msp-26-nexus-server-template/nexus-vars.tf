a0925828eed41dd6404c87d0cdad4420f5c39d7b
ahmet <ahmtakn@protonmail.com>
2022-11-03 21:37:57 +0000

    added Nexus server terraform files

---
+++ infrastructure/msp-26-nexus-server-template/nexus-vars.tf
@@ -0,0 +1,8 @@
+variable "petclinic-rancher" {}
+variable "ami" {
+  description = "amazon linux 2 ami"
+}
+variable "region" {}
+variable "instance_type" {}
+variable "nexus_server_secgr" {}
+variable "nexus-server-tag" {}
